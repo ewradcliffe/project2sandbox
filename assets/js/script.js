140aff0d19e14bb73fc2e73372905b8182ce654f
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-21 15:00:57 +0000

    Added placeholder Javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 
+
+
+function handleSubmit(event) {
+    // Prevent the default submit action)
+    event.preventDefault();
+
+    // Get the input elements
+    let name = document.getElementById('name-input')
+
+    // Log their values to the console
+    console.log('name', name.value);
+}
+
+// This gets the form and attaches an event listener to it
+let form = document.getElementById('name-input');
+form.addEventListener('submit', handleSubmit);
+
+
 /*Basic game function */
 function game(answer) {
+    event.preventDefault();
     if (answer === 'true') {
         console.log("Correct");
     } else {
         console.log("wrong");
     }
 }
+
+let answer = document.getElementsByClassName('answer');
+console.log(answer);
+
+
+// This gets the form and attaches an event listener to it
+let questionAnswer = document.getElementById('question');
+console.log(questionAnswer);
+questionAnswer.addEventListener('submit', game);
