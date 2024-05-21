1de4b89312bbcf5ef833fa8a4a8b1dd7a0dbadf4
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-21 19:11:42 +0000

    Added function to validate code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 function printName(event) {
     event.preventDefault();
     let name = document.getElementById('name');
-    console.log(name);
     let printName = document.getElementById("print-name");
     printName.innerHTML = `Hi ${name.value}, Welcome to the game!`;
 }
 
+function checkAnswer(event) {
+    event.preventDefault();
+    if(document.getElementById('option-three').checked) {   
+        let answer = document.getElementsByClassName('answer').value;  
+        let printAnswer = document.getElementById("print-answer");
+        printAnswer.innerHTML = "the answer is correct!"   
+    }  else {
+        let printAnswer = document.getElementById("print-answer");
+        printAnswer.innerHTML = "the answer is wrong ;(" 
+    }
+}    
+
+
 let yourName = document.getElementById("your-name");
 yourName.addEventListener('submit', printName);
 
+let currentQuestion = document.getElementById('question');
+currentQuestion.addEventListener('submit', checkAnswer);
