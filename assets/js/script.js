9d087c760dc87d89472c0229ec020cfcce68c79e
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-23 09:24:30 +0000

    added question to print question function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ function printName(event) {
     printName.innerHTML = `Hi ${name.value}, Welcome to the game!`;
 }
 
-/*Event Listener to print the question*/
+/*Function to print the question*/
 function printQuestion(event) {
     let printMyQuestion = document.getElementById('quiz-area');
-    printMyQuestion.innerHTML = 'This finally worked!!'
+    printMyQuestion.innerHTML = '<form id="question"><h3>Question 1</h3><p>What is the capital of the UK?</p><form method="POST" name="question" class="question"><input type="radio" id="option-two" name="question"><label for="option-one">Belfast</label><input type="radio" id="option-two" name="question"><label for="option-two" >Cardiff</label><input type="radio" id="correct" name="question"><label for="correct">London</label><input type="radio" id="option-four" name="question"><label for="option-four">Edinburgh</label><input type="submit" value="Submit answer"></form></form>'
+
 }
 
 
@@ -20,15 +21,15 @@ function printQuestion(event) {
 function checkAnswer(event) {
     event.preventDefault();
     let printAnswer = document.getElementById("print-answer");
-    if(document.getElementById('correct').checked) {   
-        printAnswer.innerHTML = "the answer is correct!" 
+    if (document.getElementById('correct').checked) {
+        printAnswer.innerHTML = "the answer is correct!"
         document.getElementById('score');
         ++totalScore;
-        score.innerHTML = `Your score is ${totalScore}`;  
-    }  else {
-        printAnswer.innerHTML = "the answer is wrong ;(" 
+        score.innerHTML = `Your score is ${totalScore}`;
+    } else {
+        printAnswer.innerHTML = "the answer is wrong ;("
     }
-}    
+}
 
 /*Event Listener to print name*/
 let yourName = document.getElementById("your-name");
